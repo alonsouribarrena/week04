f7e61a17533efffbbbc90cc1575e6de41fa9c6be
alonsouribarrena <uribarrena@gmail.com>
2023-08-09 22:11:07 +0200

    query bonanza

---
+++ a jQuery Bonanza/js.js
@@ -0,0 +1,11 @@
+$('#fade').click(function () {
+  $('#fade-text').fadeOut('fast')
+})
+
+$('#append').click(function () {
+  $('#append-text').append('<h3>this is the appended text<h3>')
+})
+
+$('#color').click(function () {
+  $('#color-text').css('color','red')
+})
